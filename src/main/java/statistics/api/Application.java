a5f8ce5f51c58d8f131c7bbac61a48960292b5a7
MadStarling <starling.damasio@gmail.com>
2019-09-23 22:41:20 -0300

    rename main class and its test  to Application

---
+++ src/main/java/statistics/api/Application.java
@@ -0,0 +1,7 @@
+package statistics.api;
+
+public class Application {
+    public static void main(String[] args) {
+        // spring app here
+    }
+}
---
+++ src/test/java/statistics/api/ApplicationTest.java
@@ -0,0 +1,7 @@
+package statistics.api;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+}
